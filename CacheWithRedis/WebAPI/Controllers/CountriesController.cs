5d0a435aa861759f90616b80bef30fa3e4b15262
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 14:09:36 +0300

    feat(webapi): countries controller created

---
+++ CacheWithRedis/WebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Countries.Queries.GetListCountry;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : BaseController
+    {
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList([FromQuery] GetListCountryQuery getListCountryQuery)
+        {
+            var response = await Mediator.Send(getListCountryQuery);
+            return Ok(response);
+        }
+    }
+}
