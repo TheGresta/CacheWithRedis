854672f1e8d7b01b3099e4f4f8c869156a91791c
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 13:50:06 +0300

    feat(application.features): added mapping profile

---
+++ CacheWithRedis/Application/Features/Cities/Profiles/CityMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Cities.Dtos;
+using Application.Features.Cities.Models;
+using AutoMapper;
+using Core.Paging;
+using Persistence.Entities;
+
+namespace Application.Features.Cities.Profiles;
+
+public class CityMappingProfile : Profile
+{
+	public CityMappingProfile()
+	{
+		CreateMap<City, CityDto>().ReverseMap();
+
+		CreateMap<IPaginate<City>, CityListModel>().ReverseMap();
+	}
+}
