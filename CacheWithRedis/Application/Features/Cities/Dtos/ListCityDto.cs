5807cad941b5553f5c9a7f12bd7f0c440f25a7af
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 13:49:40 +0300

    feat(application.features): added dtos

---
+++ CacheWithRedis/Application/Features/Cities/Dtos/ListCityDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Features.Cities.Dtos;
+
+public class CityDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CountryId { get; set; }
+    public string CountryName { get; set; }
+    public DateTime LastUpdate { get; set; }
+}
---
+++ CacheWithRedis/Application/Features/Cities/Models/CityListModel.cs
@@ -0,0 +1,9 @@
+﻿using Application.Features.Cities.Dtos;
+using Core.Paging;
+
+namespace Application.Features.Cities.Models;
+
+public class CityListModel : BasePageableModel
+{
+    public IList<CityDto> Items { get; set; }
+}
