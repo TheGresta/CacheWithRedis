27fc0d8f74a49f771fdd999fca770ad118127448
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 13:51:03 +0300

    feat(webapi): city controller created

---
+++ CacheWithRedis/WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Cities.Queries.GetByIdCity;
+using Application.Features.Cities.Queries.GetListCity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : BaseController
+    {
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetByID([FromQuery]GetByIdCityQuery getByIdCityQuery)
+        {
+            var response = await Mediator.Send(getByIdCityQuery);
+            return Ok(response);
+        }
+
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList([FromQuery] GetListCityQuery getListCityQuery)
+        {
+            var response = await Mediator.Send(getListCityQuery);
+            return Ok(response);
+        }
+    }
+}
