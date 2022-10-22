ce3c9346f6b31c0b71de92d920c8e14e3641b11a
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 11:13:45 +0300

    feat(persistence): add country-city entities

---
+++ CacheWithRedis/Persistence/Entities/City.cs
@@ -0,0 +1,20 @@
+﻿namespace Persistence.Entities;
+
+public class City
+{
+    public int CityId { get; set; }
+    public string Name { get; set; }
+    public int CountryId { get; set; }
+    public virtual Country? Country { get; set; }
+
+    public City()
+    {
+    }
+
+    public City(int cityId, string name, int countryId) : this()
+    {
+        CityId = cityId;
+        Name = name;
+        CountryId = countryId;
+    }
+}
---
+++ CacheWithRedis/Persistence/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entity;
+using Microsoft.VisualBasic;
+
+namespace Persistence.Entities;
+
+public class Country : BaseEntity
+{
+    public int CountryId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<City>? Cities { get; set; }
+
+    public Country()
+    {
+    }
+
+    public Country(int countryId, string name) : this()
+    {
+        CountryId = countryId;
+        Name = name;
+    }
+}
+
