d0cc8f2e2c81c1ad85955681fd02086d1dbc0854
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 13:50:27 +0300

    refactor(persistence): dbcontext update

--- CacheWithRedis/Persistence/Context/BaseDbContext.cs
+++ CacheWithRedis/Persistence/Context/BaseDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Persistence.Entities;
+using System.Reflection;
 
 namespace Persistence.Context;
 
@@ -15,6 +16,41 @@ public class BaseDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<City>(builder =>
+        {
+            builder.ToTable("city")
+            .HasKey(c => c.Id);
+
+            builder.HasOne(c => c.Country);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("city_id");
+
+            builder.Property(c => c.Name)
+                .HasColumnName("city");
+
+            builder.Property(c => c.CountryId)
+                .HasColumnName("country_id");
+
+            builder.Property(c => c.LastUpdate)
+                .HasColumnName("last_update");
+        });
+
+        modelBuilder.Entity<Country>(builder => {
+            builder.ToTable("country")
+            .HasKey(c => c.Id);
+
+            builder.HasMany(c => c.Cities);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("country_id");
+
+            builder.Property(c => c.Name)
+                .HasColumnName("country");
+
+            builder.Property(c => c.LastUpdate)
+                .HasColumnName("last_update");
+        });
     }
 }
 
