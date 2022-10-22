51e462f9bb218452f4fa3cdf920b5f4ca31b6c82
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 11:40:32 +0300

    feat(persistence): added basedbcontext

---
+++ CacheWithRedis/Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Entities;
+
+namespace Persistence.Context;
+
+public class BaseDbContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Country> Countries { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
+
