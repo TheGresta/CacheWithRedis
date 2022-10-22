5003a76d0d502f52273e0a4b585db3cbf500291a
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 12:13:57 +0300

    feat(application): added GetByIdCityQuery

---
+++ CacheWithRedis/Application/Features/Cities/Queries/GetByIdCity/GetByIdCityQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Cities.Dtos;
+using Application.Services;
+using AutoMapper;
+using MediatR;
+using Persistence.Entities;
+
+namespace Application.Features.Cities.Queries.GetByIdCity;
+
+public class GetByIdCityQuery : IRequest<CityDto>
+{
+    public int Id { get; set; }
+    public class GetByIdCityQueryHandler : IRequestHandler<GetByIdCityQuery, CityDto>
+    {
+        private readonly ICityRepository _cityRepository;
+        private readonly IMapper _mapper;
+
+        public GetByIdCityQueryHandler(ICityRepository cityRepository, IMapper mapper)
+        {
+            _cityRepository = cityRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CityDto> Handle(GetByIdCityQuery request, CancellationToken cancellationToken)
+        {
+            City city = await _cityRepository.GetAsync(c => c.Id == request.Id);
+
+            CityDto mappetCity = _mapper.Map<CityDto>(city);
+
+            return mappetCity;
+        }
+    }
+}
