8e53c9126821cdbe3d8064fa4725c2cc50e00083
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-22 14:09:17 +0300

    feat(application): getListCountryQuery created

---
+++ CacheWithRedis/Application/Features/Countries/Queries/GetListCountry/GetListCountryQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.Countries.Models;
+using Application.Services;
+using AutoMapper;
+using Core.Paging;
+using Core.Requests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Entities;
+
+namespace Application.Features.Countries.Queries.GetListCountry;
+
+public class GetListCountryQuery : IRequest<CountryListModel>
+{
+    public PageRequest PageRequest { get; set; }
+    public class GetListCountryQueryHandler : IRequestHandler<GetListCountryQuery, CountryListModel>
+    {
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMapper _mapper;
+
+        public GetListCountryQueryHandler(ICountryRepository countryRepository, IMapper mapper)
+        {
+            _countryRepository = countryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CountryListModel> Handle(GetListCountryQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<Country> countries = await _countryRepository.GetListAsync(include: x => x.Include(c => c.Cities),
+                                                                                 index: request.PageRequest.Page,
+                                                                                 size: request.PageRequest.PageSize,
+                                                                                 enableTracking: false,
+                                                                                 orderBy: g => g.OrderBy(g => g.Id));
+
+            CountryListModel mappedCountryListModel = _mapper.Map<CountryListModel>(countries);
+
+            return mappedCountryListModel;
+        }
+    }
+}
