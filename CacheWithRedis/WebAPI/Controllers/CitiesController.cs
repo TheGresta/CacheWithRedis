722ac75ff0e020d03186a07bd4e060d6f1cad77a
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-23 15:48:07 +0300

    feat(webapi): done

--- CacheWithRedis/WebAPI/Controllers/CitiesController.cs
+++ CacheWithRedis/WebAPI/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Features.Cities.Queries.GetByIdCity;
+﻿using Application.Features.Cities.Commands.DeleteCity;
+using Application.Features.Cities.Queries.GetByIdCity;
 using Application.Features.Cities.Queries.GetListCity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,13 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> GetList([FromQuery] GetListCityQuery getListCityQuery)
         {
             var response = await Mediator.Send(getListCityQuery);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] DeleteCityCommand deleteCityCommand)
+        {
+            var response = await Mediator.Send(deleteCityCommand);
             return Ok(response);
         }
     }
